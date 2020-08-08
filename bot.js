f158fa3bdaf2caf9170f50ba6e3aefd6c245475b
monkfish <m0nkfish-github@alexfoxgill.com>
2020-08-08 09:44:22 +0100

    Log

--- bot.js
+++ bot.js
@@ -3,11 +3,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  client.user.setActivity('wittybotting', { type: 'PLAYING' })
+  console.log('ready!')
 });
 
 client.on('message', message => {
   if (message.content === 'ping') {
+    console.log('received ping')
     message.reply('pong');
   }
 });
