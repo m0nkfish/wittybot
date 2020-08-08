8fc2968cf4006c2eec90b3aaf365b26e78438219
monkfish <m0nkfish-github@alexfoxgill.com>
2020-08-08 09:41:13 +0100

    Poke

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log('I am ready!');
+  client.user.setActivity('wittybotting', { type: 'PLAYING' })
 });
 
 client.on('message', message => {
