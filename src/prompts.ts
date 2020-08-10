4eb09c27fd1728386ffb15ec435e7e8b160ddc53
monkfish <m0nkfish-github@alexfoxgill.com>
2020-08-10 12:45:14 +0100

    Deal with windows resources

--- src/prompts.ts
+++ src/prompts.ts
@@ -7,6 +7,7 @@ function resource(name: string) {
   return readFileSync(path.resolve(process.cwd(), 'resources', name), 'utf8')
     .replace(/_____/g, '\\_\\_\\_\\_\\_')
     .split('\n')
+    .map(s => s.replace(/\n|\r/g, '')) // some of the resources originated in windows...
 }
 
 const all = [
@@ -35,5 +36,4 @@ export function choosePrompt(users: string[]) {
   }
   
   return template(pick(mt, resource('TestPrompts.txt')))
-    .replace(/\n/g, '')
 }
