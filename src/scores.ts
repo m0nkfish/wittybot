083c07d7e547c65ef48113c77b9e9ae16ba41539
monkfish <m0nkfish-github@alexfoxgill.com>
2020-08-12 09:55:50 +0100

    Change scoring

--- src/scores.ts
+++ src/scores.ts
@@ -2,18 +2,18 @@ import * as Discord from 'discord.js';
 import { EmbedMessage } from './actions';
 
 export class Score {
-  constructor(readonly points: number, readonly ofPossible: number) {}
+  constructor(readonly points: number, readonly ofPossible: number, readonly games: number) {}
 
   add(other: Score): Score {
-    return new Score(this.points + other.points, this.ofPossible + other.ofPossible)
+    return new Score(this.points + other.points, this.ofPossible + other.ofPossible, this.games + other.games)
   }
 
-  get ratio() {
-    return this.points / this.ofPossible
+  get rating() {
+    return this.games * this.points / this.ofPossible
   }
 
   static empty(): Score {
-    return new Score(0, 0)
+    return new Score(0, 0, 0)
   }
 }
 
@@ -31,7 +31,7 @@ export class Scores {
   }
 
   inOrder() {
-    return Array.from(this.map).sort(([, a], [, b]) => b.ratio - a.ratio)
+    return Array.from(this.map).sort(([, a], [, b]) => b.rating - a.rating)
   }
 
   static empty(): Scores {
@@ -41,12 +41,11 @@ export class Scores {
   show(channel: Discord.TextChannel | Discord.DMChannel) {
     const positiveScoresInOrder = this.inOrder()
       .filter(([, score]) => score.points > 0)
-      .map(([user, score]) => `${score.points}/${score.ofPossible} points: ${user.username}`)
 
     const description = 
       positiveScoresInOrder.length === 0
         ? `Nobody has scored since the bot was last restarted (start a game with the **!witty** command)`
-        : `The scores (since the bot was last restarted!) are:\n` + positiveScoresInOrder.join('\n')
+        : `The scores are:\n` + renderTable(positiveScoresInOrder)
 
     return EmbedMessage(channel, new Discord.MessageEmbed()
       .setTitle(`Scores on the doors...`)
@@ -55,6 +54,31 @@ export class Scores {
 
   static fromRound(arr: Array<[Discord.User, number]>): Scores {
     const available = arr.length - 1 // can't self-vote
-    return new Scores(new Map(arr.map(([user, points]) => [user, new Score(points, available)])))
+    return new Scores(new Map(arr.map(([user, points]) => [user, new Score(points, available, 1)])))
   }
+}
+
+function renderTable(scores: [Discord.User, Score][]): string {
+  return asciiTable([
+    ["User", "Games played", "Points scored", "Points possible", "Ratio"],
+    ...scores.map(([user, score]) => [user.username, score.games.toString(), score.points.toString(), score.ofPossible.toString()])
+  ], true)
+}
+
+function asciiTable(rows: string[][], firstRowHeader: boolean): string {
+  const columnWidths = rows.reduce((acc, row) => acc.map((n, i) => Math.max(n, row[i].length)), rows[0].map(_ => 0))
+
+  return [
+    `╔${columnWidths.map(w => '═'.repeat(w + 2)).join('╦')}╗`,
+    ...rows.map((r, i) => {
+      const row = `║${r.map((v, j) => ` ${v.padEnd(columnWidths[j], ' ')} `).join('╬')}║`
+      if (i === 0 && firstRowHeader) {
+        const border = `╠${columnWidths.map(w => '═'.repeat(w + 2)).join('╬')}╣`
+        return row + '\n' + border
+      }
+      return row
+    }),
+    `╚${columnWidths.map(w => '═'.repeat(w + 2)).join('╩')}╝`
+    ]
+    .join('\n')
 }
