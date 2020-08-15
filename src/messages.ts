268613078870ef8f7aa1504fd9111a1ac6f15b33
monkfish <m0nkfish-github@alexfoxgill.com>
2020-08-15 17:15:55 +0100

    Remove Envelope

--- src/messages.ts
+++ src/messages.ts
@@ -3,18 +3,6 @@ import { Score } from './scores';
 
 export type Destination = Discord.TextChannel | Discord.User
 
-export class Envelope {
-  constructor(readonly destination: Destination, readonly message: Message) {}
-
-  send() {
-    const content = this.message.content
-    if (content instanceof Discord.MessageEmbed) {
-      content.setColor('#A4218A')
-    }
-    return this.destination.send(this.message.content)
-  }
-}
-
 export interface Message {
   content: string | Discord.MessageAdditions
 }
