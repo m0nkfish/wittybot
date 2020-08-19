cb231919f91c857c5752d478f6fe9fa9b962dbf3
monkfish <m0nkfish-github@alexfoxgill.com>
2020-08-19 16:21:04 +0100

    Remove message delete logging

--- src/bot.ts
+++ src/bot.ts
@@ -14,41 +14,14 @@ client.on('ready', () => {
     }
   })
   console.log('ready!')
-  const channel = client.channels.cache.find(ch => ch instanceof Discord.TextChannel && ch.name === 'wittybot') as Discord.TextChannel | undefined
-  if (!channel) {
-    return
+  for (const [_, guild] of client.guilds.cache) {
+    for (const [_, channel] of guild.channels.cache) {
+      if (channel instanceof Discord.TextChannel && channel.name === 'wittybot') {
+        engine.interpret(Send(channel, new ReleaseMessage(testMode)))
+      }
+    }
   }
-  engine.interpret(Send(channel, new ReleaseMessage(testMode)))
 });
-
-client.on('messageDelete', async message => {
-  if (message.author && message.author === client.user && message.guild && message.channel instanceof Discord.TextChannel) {
-    const fetchedLogs = await message.guild.fetchAuditLogs({
-      limit: 1,
-      type: 'MESSAGE_DELETE',
-    });
-    // Since we only have 1 audit log entry in this collection, we can simply grab the first one
-    const deletionLog = fetchedLogs.entries.first();
-
-    // Let's perform a coherence check here and make sure we got *something*
-    if (!deletionLog) {
-      console.log(`A ${message.author.tag} message was deleted by a mystery user.`);
-      return
-    }
-
-    // We now grab the user object of the person who deleted the message
-    // Let us also grab the target of this action to double check things
-    const { executor, target } = deletionLog;
-
-    // And now we can update our output with a bit more information
-    // We will also run a check to make sure the log we got was for the same author's message
-    if (target instanceof Discord.User && target === message.author) {
-      console.log(`A ${message.author.tag} message was deleted by <@${executor.id}>.`);
-    } else {
-      console.log(`A ${message.author.tag} message was deleted by a mystery user.`);
-    }
-  }
-})
 
 const testMode = process.env.TEST_MODE === "true"
 
