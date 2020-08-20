51cf6df29519ab84777dfd770264e7c97e127759
monkfish <m0nkfish-github@alexfoxgill.com>
2020-08-20 23:30:19 +0100

    Stop logging submissions

--- src/round.ts
+++ src/round.ts
@@ -31,7 +31,6 @@ export class RoundScoreView {
         voters,
         voted
       ))
-      console.log(`SubmissionScoreView(${sub.id}, ${sub.text}, ${submitter}, ${voters.length}[], ${voted})`)
     }
 
     return new RoundScoreView(id, filledPrompt, submissions)
