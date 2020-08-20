c0a68281ebd4c886328a5f9d8eedeee91591e4dc
monkfish <m0nkfish-github@alexfoxgill.com>
2020-08-20 18:01:47 +0100

    Improve help message

--- src/messages.ts
+++ src/messages.ts
@@ -41,17 +41,16 @@ export class HelpMessage implements Message {
       .addField('How to play', [
         `1. Someone starts a game with the \`!witty\` command`,
         `2. The bot sends a prompt to the channel`,
-        `3. Players have a certain amount of time to DM the bot a submission completing the prompt with the funniest thing they can think of`,
-        `4. After submissions are in and the time's up, players DM the bot their vote for the funniest entry`,
-        `5. Repeat ad infinitum`
+        `3. Players have a certain amount of time to submit the funniest thing they can think of (either DM the bot or use \`||spoiler||\` tags in-channel)`,
+        `4. After submissions are in and the time's up, players vote for the funniest entry`,
+        `5. Repeat ad infinitum (or until there aren't enough players)`
       ])
       .addField('Commands', [
         `\`!help\` - you're looking at it`,
-        `\`!witty\` - start a new game`,
+        `\`!witty [timeout]\` - start a new game. timeout is the number of seconds per round (defaults to 60)`,
         `\`!skip\` - skip the current prompt`,
         `\`!notify\` - be notified when a new game starts`,
-        `\`!unnotify\` - stop being notified when a new game starts`,
-        `\`!scores\` - view the scoreboard`
+        `\`!unnotify\` - stop being notified when a new game starts`
       ])
   }
 }
