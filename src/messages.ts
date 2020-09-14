88101dde855d69a8c7da5997309303e86b2e03db
monkfish <m0nkfish-github@alexfoxgill.com>
2020-09-14 22:40:03 +0100

    Fun message if two people are each others only votes

--- src/messages.ts
+++ src/messages.ts
@@ -6,6 +6,7 @@ import { Id } from './id';
 import { shuffle } from 'random-js';
 import { mt } from './random';
 import { not } from 'fp-ts/lib/function';
+import { pairs, arrayEq } from './util';
 
 export type Destination = Discord.TextChannel | Discord.User
 
@@ -158,13 +159,38 @@ export class VotingFinishedMessage implements Message {
     readonly prompt: Prompt,
     readonly withVotes: Array<{ user: Discord.User, votes: Discord.User[], voted: boolean, submission: string }>) {}
 
-  get content() {
-    let title = `The votes are in!`
+  sweep(): Discord.User | undefined {
     const sweep = this.withVotes.find(x => x.voted && x.votes.length === this.withVotes.length - 1)
     if (sweep) {
-      title = title + ` ${sweep.user.username} sweeps the board!`
-    } else if (this.withVotes.every(v => v.voted && v.votes.length === 1)) {
-      title = title + ` It's a ${this.withVotes.length}-way split!`
+      return sweep.user
+    }
+  }
+  
+  split(): number | undefined {
+    if (this.withVotes.every(v => v.voted && v.votes.length === 1)) {
+      return this.withVotes.length
+    }
+  }
+
+  pals(): [Discord.User, Discord.User] | undefined {
+    for (const [a, b] of pairs(this.withVotes)) {
+      if (arrayEq(a.votes, [b.user]) && arrayEq([a.user], b.votes)) {
+        return [a.user, b.user]
+      }
+    }
+  }
+
+  get content() {
+    let title = `The votes are in!`
+    const sweep = this.sweep()
+    const split = this.split()
+    const pals = this.pals()
+    if (sweep) {
+      title = title + ` :trophy: ${sweep.username} sweeps the board!`
+    } else if (split) {
+      title = title + ` :bowling: It's a ${this.withVotes.length}-way split!`
+    } else if (pals) {
+      title = title + ` :revolving_hearts: ${pals[0].username} and ${pals[1].username} sitting in a tree, V-O-T-I-N-G (for each other)`
     }
 
     return new Discord.MessageEmbed()
--- src/util.ts
+++ src/util.ts
@@ -21,3 +21,29 @@ export function getOrSet<Key, Value>(map: Map<Key, Value>, key: Key, value: () =
 export function clamp(x: number, min: number, max: number) {
   return Math.max(min, Math.min(x, max))
 }
+
+export function* pairs<T>(array: T[]): Generator<readonly [T, T]> {
+  if (array.length < 2) {
+    return
+  }
+
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      yield [array[i], array[j]] as const
+    }
+  }
+}
+
+export function arrayEq<T>(a: T[], b: T[]): boolean {
+  if (a.length !== b.length) {
+    return false
+  }
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false
+    }
+  }
+
+  return true
+}
