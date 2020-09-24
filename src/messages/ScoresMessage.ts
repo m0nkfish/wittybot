e85a2191fc71e24e45fc3e587d7c8d5d110f2a1a
monkfish <m0nkfish-github@alexfoxgill.com>
2020-09-24 22:10:15 +0100

    Score description trim

--- src/messages/ScoresMessage.ts
+++ src/messages/ScoresMessage.ts
@@ -10,9 +10,13 @@ export class ScoresMessage implements Message {
   get content() {
     const description =
       this.positiveScoresInOrder.length === 0
-        ? `Nobody has scored! Start a game with \`!witty\``
-        : `Current rating formula: \`\`\`score_per_round = points_score * min(points_available / 4, 1)\ntotal_score = score_per_round / max(games_played, 20)\`\`\``
-
+        ? [`Nobody has scored! Start a game with \`!witty\``]
+        : this.positiveScoresInOrder.slice(0, 15).flatMap(([user, score], i) => [
+          `${i + 1}. ${emoji(i)}${mention(user)}: **${score.rating.toFixed(2)}**`,
+          `${score.totalPoints} points/${score.totalPossible} possible; ${score.games} games; ${score.gamesRatio} points per game`,
+          ``
+        ])
+        
     const emoji = (place: number) =>
       place === 0 ? ':first_place: '
         : place === 1 ? ':second_place: '
@@ -21,14 +25,7 @@ export class ScoresMessage implements Message {
 
     return new Discord.MessageEmbed()
       .setTitle(`:trophy: Scores ${this.timeframe}`)
-      .setDescription([
-        ...this.positiveScoresInOrder.slice(0, 15).flatMap(([user, score], i) => [
-          `${i + 1}. ${emoji(i)}${mention(user)} with a rating of **${score.rating.toFixed(2)}**`,
-          `${score.totalPoints} points of a possible ${score.totalPossible} (${score.ratio}), over ${score.games} games (${score.gamesRatio} points per game)`,
-          ``
-        ]),
-        description
-      ])
-      .addFields()
+      .setDescription(description)
+      .addField('Current rating formula', `\`\`\`score_per_round = points_score * min(points_available / 4, 1)\ntotal_score = score_per_round / max(games_played, 20)\`\`\``)
   }
 }
