723f302cf0d1996d48e1510ef7d9d640e84b4fdb
monkfish <m0nkfish-github@alexfoxgill.com>
2021-02-13 15:32:04 +0000

    Switch in/out mistake

--- src/witty/command-handlers/out.ts
+++ src/witty/command-handlers/out.ts
@@ -1,16 +1,21 @@
 import { CommandHandler } from '../../command-handler';
 import { StartingState } from '../state';
-import { In } from '../commands';
-import { CompositeAction, NewState, OptionalAction } from '../actions'
+import { IdleState } from '../../state'
+import { Out } from '../commands';
+import { CompositeAction, NewState, Send } from '../actions'
+import { BasicMessage } from '../../messages';
 
 export const OutHandler = new CommandHandler((state, command) => {
-  if (state instanceof StartingState && command.type === In.type) {
-    if (!state.interested.some(x => x === command.member.user)) {
-      const interested = [...state.interested, command.member.user]
-      return CompositeAction(
-        NewState(new StartingState(state.context, interested)),
-        OptionalAction(interested.length === Math.max(state.context.minPlayers, 5) && state.begin())
-      )
+  if (state instanceof StartingState && command.type === Out.type) {
+    if (state.interested.some(x => x === command.member.user)) {
+      const interested = state.interested.filter(x => x !== command.member.user)
+      if (interested.length === 0) {
+        return CompositeAction(
+          NewState(new IdleState(state.context.guildCtx)),
+          Send(state.context.channel, new BasicMessage(`Witty game cancelled`)))
+      } else {
+        return NewState(new StartingState(state.context, state.interested.filter(x => x !== command.member.user)))
+      }
     }
   }
 })
