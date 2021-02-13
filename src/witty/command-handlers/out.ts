4d52cbd74b571a915a2701f98c7b6648a652d59a
monkfish <m0nkfish-github@alexfoxgill.com>
2021-02-13 16:15:13 +0000

    Debug command handlers

--- src/witty/command-handlers/out.ts
+++ src/witty/command-handlers/out.ts
@@ -4,9 +4,11 @@ import { IdleState } from '../../state'
 import { Out } from '../commands';
 import { CompositeAction, NewState, Send } from '../actions'
 import { BasicMessage } from '../../messages';
+import { log } from '../../log';
 
 export const OutHandler = CommandHandler.sync((state, command) => {
   if (state instanceof StartingState && command.type === Out.type && state.isInterested(command.member.user)) {
+    log('out_handler');
     const nextState = state.removeInterested(command.member.user)
     return nextState.interested.length === 0
       ? CompositeAction(
