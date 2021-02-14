221fd20c9549b475c9ce2bae583693efa3498b99
monkfish <m0nkfish-github@alexfoxgill.com>
2021-02-14 09:29:15 +0000

    Defer more

--- src/commands/all.ts
+++ src/commands/all.ts
@@ -2,8 +2,8 @@ import { AllWittyCommandHandlers } from '../witty/command-handlers';
 import { AllWittyCommands } from '../witty/commands';
 import { HelpCommandHandler, HelpCommandFactory } from './help';
 
-export const AllScopedCommandHandlers = AllWittyCommandHandlers
-export const AllScopedCommandFactories = AllWittyCommands
+export const AllScopedCommandHandlers = () => AllWittyCommandHandlers()
+export const AllScopedCommandFactories = () => AllWittyCommands()
 
-export const AllGlobalCommandHandlers = HelpCommandHandler
-export const AllGlobalCommandFactories = HelpCommandFactory
+export const AllGlobalCommandHandlers = () => HelpCommandHandler()
+export const AllGlobalCommandFactories = () => HelpCommandFactory()
