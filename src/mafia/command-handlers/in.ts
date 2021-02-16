b283c01782c44b8eb98a17ef7a3b916879dc8365
monkfish <m0nkfish-github@alexfoxgill.com>
2021-02-16 17:08:21 +0000

    Cap max players at sign up

--- src/mafia/command-handlers/in.ts
+++ src/mafia/command-handlers/in.ts
@@ -1,9 +1,10 @@
 import { CommandHandler, In } from '../../commands';
 import { StartingState } from '../state';
 import { NewState } from '../../actions'
+import { MaxPlayers } from '../constants';
 
 export const InHandler = () => CommandHandler.build.state(StartingState).command(In).sync((state, command) => {
-  if (!state.isInterested(command.member.user)) {
+  if (!state.isInterested(command.member.user) && state.interested.length < MaxPlayers) {
     const nextState = state.addInterested(command.member.user)
     return NewState(nextState)
   }
--- src/mafia/constants.ts
+++ src/mafia/constants.ts
@@ -3,6 +3,6 @@ import { CommandReacts } from "./messages/text"
 
 export const StartingStateDelay = Duration.minutes(3)
 export const MinPlayers = 5
-export const MaxPlayers = CommandReacts.length - 1
+export const MaxPlayers = CommandReacts.length
 export const NightDuration = Duration.minutes(1)
 export const DayDuration = Duration.minutes(1)
