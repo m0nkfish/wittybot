b1f17d8b55b0319dbad4e72213def544cf3e40ea
monkfish <m0nkfish-github@alexfoxgill.com>
2021-02-18 23:01:14 +0000

    Log command name

--- src/commands/scoped-command-handler.ts
+++ src/commands/scoped-command-handler.ts
@@ -1,9 +1,9 @@
+import { Action } from '../actions';
 import { CaseFactory, isCase } from '../case';
 import { log } from '../log';
 import { AnyGameState } from '../state';
-import { Action } from '../actions';
-import { ScopedCommand } from './command';
 import { Constructor, isAny, isType } from '../util';
+import { ScopedCommand } from './command';
 
 export class CommandHandler {
   constructor(public handle: (state: AnyGameState, command: ScopedCommand) => Promise<Action | undefined>) { }
@@ -14,29 +14,28 @@ export class CommandHandler {
   static sync = (handle: (state: AnyGameState, command: ScopedCommand) => Action | undefined) =>
     new CommandHandler(async (s, c) => handle(s, c))
 
-  static get build() { return new CommandHandlerBuilder(isAny, null, isAny, null) }
+  static get build() { return new CommandHandlerBuilder(isAny, null, isAny) }
 }
 
 export class CommandHandlerBuilder<State extends AnyGameState, Cmd extends ScopedCommand> {
   constructor(
     private readonly checkState: (state: AnyGameState) => state is State,
     private readonly stateName: string | null,
-    private readonly checkCommand: (command: ScopedCommand) => command is Cmd,
-    private readonly commandName: string | null) {}
+    private readonly checkCommand: (command: ScopedCommand) => command is Cmd) {}
 
   sync = (handle: (state: State, command: Cmd) => Action | undefined) =>
     this.async(async (s, h) => handle(s, h))
 
   async = (handle: (state: State, command: Cmd) => Promise<Action | undefined>) => new CommandHandler(async (state, command) => {
     if (this.checkState(state) && this.checkCommand(command)) {
-      log('handling_command', { state: this.stateName, command: this.commandName })
+      log('handle-command', { state: this.stateName, command: command.type })
       return handle(state, command)
     }
   })
 
   command = <Cmd extends ScopedCommand>(commandType: CaseFactory<Cmd>) =>
-    new CommandHandlerBuilder<State, Cmd>(this.checkState, this.stateName, isCase(commandType), commandType.type)
+    new CommandHandlerBuilder<State, Cmd>(this.checkState, this.stateName, isCase(commandType))
 
   state = <State extends AnyGameState>(state: Constructor<State>) =>
-    new CommandHandlerBuilder<State, Cmd>(isType(state), state.name, this.checkCommand, this.commandName)
+    new CommandHandlerBuilder<State, Cmd>(isType(state), state.name, this.checkCommand)
 }
